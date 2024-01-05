0ad926d68f32da2d35b95ee1ec94f8370c8502ed
EdsonMartinelli <martinelli1997@gmail.com>
2024-01-05 16:13:38 -0300

    fix: turn hashmap a independent library

--- test/tests/HashMap/build.sh
+++ test/tests/HashMap/build.sh
@@ -1,2 +1,2 @@
-gcc TestHashMap.c ../../Unity/unity.c ../../../output/lib/libhashmap.a ../../../output/lib/liblinkedlist.a -o TestHashMap.out
+gcc TestHashMap.c ../../Unity/unity.c ../../../output/lib/libhashmap.a  -o TestHashMap.out
 ./TestHashMap.out
