310a97dcef805df62811eb6a5394ebacb37e7170
EdsonMartinelli <martinelli1997@gmail.com>
2024-01-12 20:54:40 -0300

    add: create correctness test for bynari tree

--- test/tests/test_binarytree.c
+++ test/tests/test_binarytree.c
@@ -19,6 +19,32 @@ void setUp()
 
 void tearDown() {}
 
+static bool verifyTreeCorrectness(BinaryTree bintree, BinaryTreeNode *node)
+{
+    if (node == NULL)
+        return true;
+
+    bool isLeftCorrect = true;
+    bool isRightCorrect = true;
+
+    if (node->left != NULL)
+        isLeftCorrect = (*bintree.comparator)(node->content, (node->left)->content) > 0 ? true : false;
+
+    if (node->right != NULL)
+        isRightCorrect = (*bintree.comparator)(node->content, (node->right)->content) <= 0 ? true : false;
+
+    if (isRightCorrect && isLeftCorrect)
+    {
+        bool isLeftTreeCorrect = verifyTreeCorrectness(bintree, node->left);
+        bool isRightTreeCorrect = verifyTreeCorrectness(bintree, node->right);
+        return isLeftTreeCorrect && isRightTreeCorrect;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 void binaryTreeShouldBeInitializedCorrecty()
 {
     TEST_ASSERT_EQUAL(0, bt.size);
@@ -35,6 +61,28 @@ void binaryTreeShouldInsertNodeCorrectyInRoot()
     TEST_ASSERT_EQUAL(1, bt.size);
     TEST_ASSERT_EQUAL_PTR(&root, bt.root);
     TEST_ASSERT_EQUAL_PTR(temp, &root);
+}
+
+void binaryTreeShouldInsertNodeCorrecty()
+{
+    int content[expSize];
+    BinaryTreeNode items[expSize];
+
+    for (int i = 0; i < expSize; i++)
+        content[i] = randomIntNumber(-100, 100, i);
+
+    for (int i = 0; i < expSize; i++)
+        items[i] = (BinaryTreeNode){.content = content + i};
+
+    for (int i = 0; i < expSize; i++)
+    {
+        BinaryTreeNode *temp = insertBinaryTree(&bt, items + i);
+        TEST_ASSERT_NOT_NULL(temp);
+        TEST_ASSERT_EQUAL_PTR(items + i, temp);
+    }
+
+    TEST_ASSERT_EQUAL(expSize, bt.size);
+    TEST_ASSERT_TRUE(verifyTreeCorrectness(bt, bt.root));
 }
 
 void binaryTreeShouldDoNothingIfInsertNodeIsNull()
@@ -78,7 +126,6 @@ void testPrintBinaryTree()
 
     for (int i = 0; i < size; i++)
         insertBinaryTree(&bt, items + i);
-
     printBinaryTree(bt);
 }
 int main()
@@ -86,9 +133,10 @@ int main()
     UNITY_BEGIN();
     RUN_TEST(binaryTreeShouldBeInitializedCorrecty);
     RUN_TEST(binaryTreeShouldInsertNodeCorrectyInRoot);
+    RUN_TEST(binaryTreeShouldInsertNodeCorrecty);
     RUN_TEST(binaryTreeShouldDoNothingIfInsertNodeIsNull);
     RUN_TEST(binaryTreeShouldDoNothingIfBinaryTreeIsNull);
     RUN_TEST(binaryTreeShouldReturnCorrectyWhenItIsEmptyOrNot);
-    RUN_TEST(testPrintBinaryTree);
+    // RUN_TEST(testPrintBinaryTree);
     return UNITY_END();
 }
