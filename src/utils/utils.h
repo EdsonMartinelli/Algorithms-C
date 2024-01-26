a66e10794717e436a9db3b30aaeea1a799914a67
EdsonMartinelli <martinelli1997@gmail.com>
2024-01-26 18:06:48 -0300

    add: utils folder

---
+++ src/utils/utils.h
@@ -0,0 +1,14 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static int randomIntNumber(int min, int max, int seedLoop)
+{
+    srand(time(0) + seedLoop);
+    return (rand() % (max - min)) + min;
+}
+
+#endif
